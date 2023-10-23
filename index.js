af9ace5a80eeb4c188f54bb0bb7094b869c20730
Jeffmwai <Jefylionel@gmail.com>
2023-10-23 19:10:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    const winningYear = arrayOfObjects.find(obj => obj.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+    return undefined;
+    }
+  }
+  
